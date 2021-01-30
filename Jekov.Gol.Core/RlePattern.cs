95f73de87350318af7e2513eee853e670c365444
Antony Jekov <antony.jekov@gmail.com>
2021-01-30 23:52:28 +0100

    cleans up rle pattern.

--- Jekov.Gol.Core/RlePattern.cs
+++ Jekov.Gol.Core/RlePattern.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Jekov.Gol.Core
 {
     public class RlePattern : IEnumerable<Location>
     {
-        private Regex _patternSchema = new Regex("^([0-9]*[b|o])[0-9ob$]*!$");
-        private Location[] _cells;
+        private const char NewLine = '$';
+        private const char Value = 'b';
+
+        private readonly ICollection<Location> _cells;
+        private readonly Regex _cellSchema = new Regex("[0-9]*[o|b]");
+        private readonly Regex _digitsSchema = new Regex(@"\d+");
+        private readonly Regex _lineSchema = new Regex(@"([0-9]*\$)|([0-9ob]*[o|b])");
+        private readonly Regex _patternSchema = new Regex("^([0-9]*[b|o])[0-9ob$]*!$");
 
         public RlePattern(string patternData)
         {
@@ -23,52 +28,6 @@ namespace Jekov.Gol.Core
             _cells = BuildPattern(patternData);
         }
 
-        private Location[] BuildPattern(string patternData)
-        {
-            var cells = new List<Location>();
-            var digits = "";
-            var x = 0;
-            var y = 0;
-
-            foreach (var ch in patternData)
-            {
-                if (char.IsDigit(ch))
-                {
-                    digits += ch;
-                    continue;
-                }
-
-                var repeat = 1;
-                if (digits.Length > 0)
-                {
-                    repeat = int.Parse(digits);
-                    digits = "";
-                }
-
-                if (ch == '$')
-                {
-                    y += repeat;
-                    x = 0;
-                }
-
-                if (ch == 'b')
-                {
-                    x += repeat;
-                }
-
-                if (ch == 'o')
-                {
-                    for (int i = 0; i < repeat; i++)
-                    {
-                        cells.Add(new Location(x++, y));
-                    }
-                }
-                
-            }
-
-            return cells.ToArray();
-        }
-
         public IEnumerator<Location> GetEnumerator()
         {
             foreach (var cell in _cells)
@@ -78,6 +37,60 @@ namespace Jekov.Gol.Core
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private IEnumerable<Location> BuildLine(string line, int y)
+        {
+            var x = 0;
+            var cells = _cellSchema.Matches(line);
+
+            foreach (Match match in cells)
+            {
+                var cell = match.Value;
+                var repeat = ExtractRepeat(cell, 1);
+
+                if (cell.EndsWith(Value))
+                {
+                    x += repeat;
+                    continue;
+                }
+
+                for (int i = 0; i < repeat; i++)
+                {
+                    yield return new Location(x++, y);
+                }
+            }
+        }
+
+        private ICollection<Location> BuildPattern(string patternData)
+        {
+            var cells = new List<Location>();
+            var lines = _lineSchema.Matches(patternData);
+            var y = 0;
+
+            foreach (Match match in lines)
+            {
+                var line = match.Value;
+
+                if (line.EndsWith(NewLine))
+                {
+                    var repeat = ExtractRepeat(line, 1);
+                    y += repeat;
+
+                    continue;
+                }
+
+                cells.AddRange(BuildLine(line, y));
+            }
+
+            return cells;
+        }
+
+        private int ExtractRepeat(string line, int defaultValue)
+        {
+            var digits = _digitsSchema.Match(line);
+
+            return digits.Success ? int.Parse(digits.Value) : defaultValue;
+        }
 
         public class DataFormatException : FormatException
         {
